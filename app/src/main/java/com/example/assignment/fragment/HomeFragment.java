acaba1004398d2a821b2b224f2a454d6977767fd
XuchongYangJerry <yang7474741@Gmail.com>
2022-04-27 09:54:49 +1000

    Jerry second commit

--- app/src/main/java/com/example/assignment/fragment/HomeFragment.java
+++ app/src/main/java/com/example/assignment/fragment/HomeFragment.java
@@ -1,86 +1,30 @@
 package com.example.assignment.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import androidx.fragment.app.Fragment;
 
-import com.example.assignment.LoginActivity;
-import com.example.assignment.Root;
-import com.example.assignment.ThirdActivity;
-import com.example.assignment.WeatherApiInterface;
 import com.example.assignment.databinding.HomeFragmentBinding;
 import com.example.assignment.viewModel.SharedViewModel;
-import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeFragment extends Fragment {
     private SharedViewModel model;
-    private HomeFragmentBinding binding;
+    private HomeFragmentBinding addBinding;
     public HomeFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the View for this fragment
-        binding = HomeFragmentBinding.inflate(inflater, container, false);
-        View view = binding.getRoot();
-
-        binding.signOutButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getActivity(), LoginActivity.class));
-        }
-        });
-
-         binding.jumpButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-        startActivity(new Intent(getActivity(), ThirdActivity.class));
-        }
-        });
-
-        // https://api.openweathermap.org/data/2.5/weather?lat=-37.813629&lon=144.963058&appid=80273ca2896861a72eca02c8f231e796
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.openweathermap.org/data/2.5/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        WeatherApiInterface weatherApiInterface = retrofit.create(WeatherApiInterface.class);
-
-        Call<Root> call = weatherApiInterface.getWeather();
-
-        call.enqueue(new Callback<Root>() {
-            @Override
-            public void onResponse(Call<Root> call, Response<Root> response) {
-                Root root = response.body();
-                double temp =  root.getTemp() - 273.15;
-                binding.tempTextView.setText("Clayton: " + String.valueOf((int)temp) + "°C");
-            }
-
-            @Override
-            public void onFailure(Call<Root> call, Throwable t) {
-                System.out.println(t.getMessage());
-            }
-        });
-
+        addBinding = HomeFragmentBinding.inflate(inflater, container, false);
+        View view = addBinding.getRoot();
         return view;
     }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+        addBinding = null;
     }
 }
