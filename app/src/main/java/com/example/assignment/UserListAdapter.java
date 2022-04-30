ec820683defe35e8c21c5eeff818b43b698e6a35
XuchongYangJerry <yang7474741@Gmail.com>
2022-04-30 18:21:13 +1000

    Jerry push

--- app/src/main/java/com/example/assignment/UserListAdapter.java
+++ app/src/main/java/com/example/assignment/UserListAdapter.java
@@ -1,5 +1,7 @@
 package com.example.assignment;
 
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -7,11 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 
+import com.example.assignment.databinding.RecyclerviewItemBinding;
 import com.example.assignment.entity.User;
 import com.example.assignment.viewModel.UserViewModel;
 
+import java.util.List;
+
 public class UserListAdapter extends ListAdapter<User, UserViewHolder> {
-    private UserViewModel userViewModel;
 
     public UserListAdapter(@NonNull DiffUtil.ItemCallback<User> diffCallback){
         super(diffCallback);
@@ -19,24 +23,28 @@ public class UserListAdapter extends ListAdapter<User, UserViewHolder> {
 
     @Override
     public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return UserViewHolder.onCreate(parent);
+        RecyclerviewItemBinding binding=
+                RecyclerviewItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new UserViewHolder(binding).onCreate(parent);
     }
 
     @Override
     public void onBindViewHolder(UserViewHolder viewHolder, int position){
         User user = getItem(position);
         viewHolder.bind(user.getUserId(), user.getFirstName(), user.getLastName());
-        UserViewHolder.binding.itemDelete.setOnClickListener(new View.OnClickListener() {
+        viewHolder.binding.itemDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userViewModel.delete(user);
+                //viewHolder.removeData(position);
                 notifyDataSetChanged();
             }
         });
     }
 
+
+
     //identify if the two users are the same
-    static class UserDiff extends DiffUtil.ItemCallback<User> {
+    public static class UserDiff extends DiffUtil.ItemCallback<User> {
 
         @Override
         public boolean areItemsTheSame(@NonNull User oldItem, @NonNull User newItem) {
