3a7e158c380657f5a3e4a50f0a1880467d5c9ce5
zguo0040 <zguo0040@student.monash.edu>
2022-05-15 18:03:36 +1000

    created the sensor function Squat up

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -15,11 +15,13 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.assignment.databinding.ActivityMainBinding;
 import com.example.assignment.fragment.HeartRateFragment;
 import com.example.assignment.fragment.KcalFragment;
+import com.example.assignment.fragment.SquatupFragment;
 import com.example.assignment.fragment.WalkActivityFragment;
 import com.example.assignment.viewModel.PlanViewModel;
 import com.facebook.CallbackManager;
@@ -31,7 +33,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding  binding;
+    private ActivityMainBinding binding;
     private AppBarConfiguration mAppBarConfiguration;
     ShareButton shareButton;
     CallbackManager callbackManager;
@@ -39,8 +41,11 @@ public class MainActivity extends AppCompatActivity {
     ShareLinkContent content = new ShareLinkContent.Builder()
             .setContentUrl(Uri.parse("https://developers.facebook.com"))
             .build();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e("Main activity", "line1");
+
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
@@ -57,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.heart_rate_fragment,
                 R.id.walk_activity_fragment,
                 R.id.kcal_fragment,
+                R.id.fragment_squatup,
                 R.id.ThirdActivity,
                 R.id.PlanListActivity,
                 R.id.MapActivity,
@@ -64,19 +70,19 @@ public class MainActivity extends AppCompatActivity {
                 //to display the Navigation button as a drawer symbol,not being shown as an Up button
                 .setOpenableLayout(binding.drawerLayout)
                 .build();
-        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
         NavHostFragment navHostFragment = (NavHostFragment)
                 fragmentManager.findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
         //Sets up a NavigationView for use with a NavController.
         NavigationUI.setupWithNavController(binding.navigationView, navController);
         //Sets up a Toolbar for use with a NavController.
-        NavigationUI.setupWithNavController(binding.appBar.toolbar,navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(binding.appBar.toolbar, navController, mAppBarConfiguration);
 
 
         binding.homeButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
+            @Override
+            public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, MainActivity.class));
             }
         });
@@ -99,95 +105,95 @@ public class MainActivity extends AppCompatActivity {
         // userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
 
         /**final ArrayAdapter<String> spinnerAdapter = new
-                ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
-        binding.movieSpinner.setAdapter(spinnerAdapter);
+         ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+         binding.movieSpinner.setAdapter(spinnerAdapter);
 
-        binding.addButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String newMovie = binding.editText.getText().toString();
-                spinnerAdapter.add(newMovie);
-                spinnerAdapter.notifyDataSetChanged();
-                binding.movieSpinner.setSelection(spinnerAdapter.getPosition(newMovie));
-            }
-        });
-        binding.clearButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //binding.editText.getText().toString();
-                binding.editText.setText("");
-            }
-        });*/
+         binding.addButton.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View v) {
+         String newMovie = binding.editText.getText().toString();
+         spinnerAdapter.add(newMovie);
+         spinnerAdapter.notifyDataSetChanged();
+         binding.movieSpinner.setSelection(spinnerAdapter.getPosition(newMovie));
+         }
+         });
+         binding.clearButton.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View v) {
+         //binding.editText.getText().toString();
+         binding.editText.setText("");
+         }
+         });*/
 
         /**binding.signOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            }
+        @Override public void onClick(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
         });
 
-        binding.jumpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ThirdActivity.class));
-            }
+         binding.jumpButton.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View view) {
+        startActivity(new Intent(MainActivity.this, ThirdActivity.class));
+        }
         });
 
-        binding.navigationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, NavigationActivity.class));
-            }
+         binding.navigationButton.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View view) {
+        startActivity(new Intent(MainActivity.this, NavigationActivity.class));
+        }
         });*/
 
         /**
          * List<User> userList = userViewModel.getAllUsersInList();
-        // {"001":{"firstName": "frank", "lastName": "Zhang", "id": "001"}}
-        Map<String, User> userMap = new HashMap<>();
-        for (User user: userList)
-        {
-            userMap.put(user.getUserId(), user);
-        }
-        Gson gson = new Gson();
-        String jsonStr = gson.toJson(userMap);
+         // {"001":{"firstName": "frank", "lastName": "Zhang", "id": "001"}}
+         Map<String, User> userMap = new HashMap<>();
+         for (User user: userList)
+         {
+         userMap.put(user.getUserId(), user);
+         }
+         Gson gson = new Gson();
+         String jsonStr = gson.toJson(userMap);
 
-        // Transform jsonStr into "Data" type
-        Data.Builder uploadPlaceBuilder = new Data.Builder();
-        Map<String, Object> placeMap = new HashMap<>();
-        placeMap.put("UserJsonStr", jsonStr);
-        uploadPlaceBuilder.putAll(placeMap);
-        Data placeInfoInputData = uploadPlaceBuilder.build();
+         // Transform jsonStr into "Data" type
+         Data.Builder uploadPlaceBuilder = new Data.Builder();
+         Map<String, Object> placeMap = new HashMap<>();
+         placeMap.put("UserJsonStr", jsonStr);
+         uploadPlaceBuilder.putAll(placeMap);
+         Data placeInfoInputData = uploadPlaceBuilder.build();
 
-        // transfer the data to work manager
-        WorkRequest saveRequest =
-                new PeriodicWorkRequest.Builder(UploadWorker.class,
-                        1, TimeUnit.DAYS,
-                        120,TimeUnit.MINUTES)
-                        .setInputData(placeInfoInputData)
-                        .build();
-        WorkManager.getInstance(this).enqueue(saveRequest);
+         // transfer the data to work manager
+         WorkRequest saveRequest =
+         new PeriodicWorkRequest.Builder(UploadWorker.class,
+         1, TimeUnit.DAYS,
+         120,TimeUnit.MINUTES)
+         .setInputData(placeInfoInputData)
+         .build();
+         WorkManager.getInstance(this).enqueue(saveRequest);
          */
     }
 
-    public void getHeartRateFragment(){
+    public void getHeartRateFragment() {
         FragmentManager fragmentmanager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
         HeartRateFragment heartRateFragment = new HeartRateFragment();
         fragmentTransaction.replace(R.id.nav_host_fragment, heartRateFragment);
         fragmentTransaction.commit();
     }
-    public void getWalkActivityFragment(){
+
+    public void getWalkActivityFragment() {
         FragmentManager fragmentmanager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
         WalkActivityFragment walkActivityFragment = new WalkActivityFragment();
         fragmentTransaction.replace(R.id.nav_host_fragment, walkActivityFragment);
         fragmentTransaction.commit();
     }
-    public void getKcalFragment(){
+
+    public void getKcalFragment() {
         FragmentManager fragmentmanager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
         KcalFragment kcalFragment = new KcalFragment();
         fragmentTransaction.replace(R.id.nav_host_fragment, kcalFragment);
         fragmentTransaction.commit();
     }
+
 
 }
---
+++ app/src/main/java/com/example/assignment/fragment/SquatupFragment.java
@@ -0,0 +1,106 @@
+package com.example.assignment.fragment;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.assignment.databinding.FragmentSquatupBinding;
+
+public class SquatupFragment extends Fragment {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    private FragmentSquatupBinding binding;
+    private double accelerationCurrentValue;
+    private double accelerationPreviousValue;
+    private int counter;
+
+    private double change;
+
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            accelerationCurrentValue = Math.sqrt(x * x + y * y + z * z);
+            change = Math.abs(accelerationCurrentValue - accelerationPreviousValue);
+            accelerationPreviousValue = accelerationCurrentValue;
+            if (change >= 3) {
+                counter += 1;
+            }
+            binding.counter.setText("" + counter);
+            binding.progressBar.setProgress(counter);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+    public SquatupFragment() {
+    }
+
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        Log.e("Squatup fragment", "2");
+        // Inflate the View for this fragment using the binding
+        binding = FragmentSquatupBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        binding.progressBar.setMax(100);//sets the maximum value 100
+//        binding.startCountButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//
+//            }
+//
+//
+//        });
+        mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        return view;
+
+
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(sensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(sensorEventListener);
+    }
+}
