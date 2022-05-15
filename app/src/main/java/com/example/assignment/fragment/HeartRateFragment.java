5574559722ab19fdf93c75c43240fb22cc72e207
XuchongYangJerry <yang7474741@Gmail.com>
2022-05-15 18:56:51 +1000

    Jerry twenty-fifth push

--- app/src/main/java/com/example/assignment/fragment/HeartRateFragment.java
+++ app/src/main/java/com/example/assignment/fragment/HeartRateFragment.java
@@ -6,10 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.assignment.databinding.FragmentHeartRateBinding;
-import com.example.assignment.viewModel.SharedViewModel;
 
 public class HeartRateFragment extends Fragment {
 
